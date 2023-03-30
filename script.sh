7e53863b31fa816cf26d5625240a5e6d7519b29a
NewReport <75560362+newreport@users.noreply.github.com>
2023-03-30 06:07:49 +0000

    .

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@ hexo c && hexo g && hexo s
 hexo c && hexo g && hexo d
 git add . && git commit -m '.' && git push
 
-lint-md source/_posts/*/* --fix
+ lint-md source/_posts/* --fix
 hexo clean && hexo g && hexo d &&  git add . && git commit -m '.' && git push
